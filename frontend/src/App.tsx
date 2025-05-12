1f4c8b89af5319363b849d8197025354c9bfeec5
j3lg <perpetualmoti0n@outlook.com>
2025-05-11 18:36:01 -0600

    fix: Corregir errores de ESLint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 // import { FaTrash } from 'react-icons/fa'; se elimna no se usa
-i
+
 
 interface Task {
   _id: string;
